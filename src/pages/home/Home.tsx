45bbb9983840a6e1eab2d98438177f148b9fa60e
Roman Afanasiev <roman.afanasiev@chisw.com>
2023-07-19 11:19:16 +0300

    add functionality for routes with create browser router

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,7 @@
-import { Link } from 'react-router-dom';
-
 export const Home = () => {
   return (
     <div className="bg-red-500 h-screen">
       <h1>Home</h1>
-      <Link to="about">About</Link>
     </div>
   );
 };
